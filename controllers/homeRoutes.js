1ddfb00ecda53c46723f536cfaf3ac448821640e
Daelen Wall <daelenwall1@gmail.com>
2023-05-08 00:09:11 -0400

    User.js winston + error logging winston

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -7,39 +7,8 @@ const winston = require('winston');
 const app = module.exports = express();
 
 
-router.get('/error', function(req, res, next) {
-  // here we cause an error in the pipeline so we see express-winston in action.
-  return next(new Error("This is an error and it should be logged to the console"));
-});
-
-router.get('/', function(req, res, next) {
-  res.write('This is a normal request, it should be logged to the console too');
-  res.end();
-});
-
-// express-winston logger makes sense BEFORE the router
-app.use(expressWinston.logger({
-  transports: [
-    new winston.transports.Console()
-  ],
-  format: winston.format.combine(
-    winston.format.colorize(),
-    winston.format.json()
-  )
-}));
-
 app.use(router);
 
-    // express-winston errorLogger makes sense AFTER the router.
-    app.use(expressWinston.errorLogger({
-      transports: [
-        new winston.transports.Console()
-      ],
-      format: winston.format.combine(
-        winston.format.colorize(),
-        winston.format.json()
-      )
-    }));
 
 router.get('/', async (req, res) => {
   try {
