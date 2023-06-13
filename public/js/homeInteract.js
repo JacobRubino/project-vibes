8269ae89c19b1062ef6acdccf0e56661399876c1
Matthew Bull <mattbull33@gmail.com>
2023-06-12 22:06:23 -0400

    good memes bad homepage

--- public/js/homeInteract.js
+++
@@ -1,9 +0,0 @@
-//user should be checked as they enter the homepage if they are logged in, if they are they get redirected to their feed
-// add event listeners and window.location ='/signin"
-
-// login screen
-document.getElementById('signinbutt').addEventListener("click", (e) => {
-e.preventDefault
-window.location.href('/signin')
-})
-
