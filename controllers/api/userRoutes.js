9da59b463d5ab2353bbc5ae1171e62828132ecd0
Jacob Rubino <jacobrubino@gmail.com>
2023-06-13 18:42:23 -0400

    fixing things

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -3,6 +3,7 @@ const router = require('express').Router();
 const { User } = require('../../models');
 
 // homepage
+
 router.post('/', async (req, res) => {
   try {
     const userData = await User.create(req.body);
